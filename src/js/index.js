b64dc8e38a3ba3281a53f960d1bb526c8a7c1360
victortorres-dev <victortorres.dev@gmail.com>
2019-12-27 19:41:22 -0600

    CREATED: I'm understanding the webpack configurations.

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+import '../css/style.scss'
+
+class Cliente {
+    constructor(nombre) {
+        this.nombre = nombre
+    }
+}
+
+const cliente = new Cliente('pedro')
+console.log(cliente)
+
+console.log('Desde el inicio')
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: {
+        index: './src/js/index.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.join(__dirname, '/dist')
+    },
+    devServer: {
+        contentBase: path.join(__dirname,'/dist'),
+        compress: true,
+        port: 9000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader'}
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader'},
+                    {loader: 'sass-loader'}
+                ]
+            }
+        ]
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'common',
+                    chunks: 'all'
+                }
+            }
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html',
+            title: 'login'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'nosotros.html',
+            template: 'src/nosotros.html',
+            title: 'Nosotros'
+        })
+    ]
+}
